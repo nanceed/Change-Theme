3c907a8eae7ab5d03179d740117f2b22e28a9afc
Nancy <duong.nancy@gmail.com>
2024-12-11 15:30:09 -0800

    Added dark theme functionality and styled with css

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function changeTheme() {
+    let body = document.querySelector("body");
+
+    // body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        body.classList.remove("dark");    
+    } else {
+        body.classList.add("dark");
+    }    
+}
+
+let themeButton = document.querySelector(".theme-button");
+themeButton.addEventListener("click", changeTheme);
